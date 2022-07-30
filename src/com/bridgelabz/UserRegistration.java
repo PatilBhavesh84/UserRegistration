402e3f2cf21407ba8aeaa0337e7e7e8878519316
PatilBhavesh84 <107094696+PatilBhavesh84@users.noreply.github.com>
2022-07-30 15:13:10 +0530

    Update UserRegistration.java

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,18 +4,26 @@ import java.util.regex.*;
 import java.util.Scanner;
 
 public class UserRegistration {
-	static void checkFirstAndLast() {
-		String nameCheck = "[A-Z]{1}[a-z]{3}";
+	String nameCheck = "[A-Z]{1}[a-z]{3}";
 		Pattern pattern = Pattern.compile(nameCheck);
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Last Name");
+		System.out.println("Enter First Name");
 
 		String name = sc.nextLine();
 
 		Matcher matcher = pattern.matcher(name);
 
 		if(matcher.matches()) {
+			System.out.println("Valid input");
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+		System.out.println("Enter last name");
+		String lName=sc.nextLine();
+		Matcher lastName =pattern.matcher(lName);
+		if(lastName.matches()) {
 			System.out.println("Valid input");
 		}
 		else {
