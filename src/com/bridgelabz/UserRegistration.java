a1785e382baf48900e7c08ed2f2fa3d1df97722b
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-30 14:22:07 +0530

    Added:User nedd to enter valid name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,9 +1,21 @@
 package com.bridgelabz;
 
+import java.util.regex.*;
+import java.util.Scanner;
+
 public class UserRegistration {
+	static void checkFormat() {
+		System.out.println("Enter first Name");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		String nameCheck = "[A-Z]{1}[a-z]{3}";
 
-	public static void main(String[] args) {
-		System.out.println("Welcome");
+		Pattern pattern = Pattern.compile(nameCheck);
+		Matcher matcher = pattern.matcher(name);
+		System.out.println(matcher.matches());
+		sc.close();
 	}
-
+	public static void main(String[] args) {
+		checkFormat();
+	}
 }
