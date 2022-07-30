81d7262a9f63fa70f2d81a4479cb89ebb18d1441
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-30 19:30:56 +0530

    Added:Password with upper case letter

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -61,7 +61,7 @@ public class UserRegistration {
 		//sc.close();
 	}
 	static void checkPassword() {
-		String passCondition="[a-z]{8}";
+		String passCondition="[A-Z]{1}[a-z]{7}";
 
 		Pattern pattern =Pattern.compile(passCondition);
 		Scanner sc =new Scanner(System.in);
