69a58584009f2b601bc60e7974cce027eb2ab6e5
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-30 15:03:32 +0530

    Added:User need to enter valid name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,18 +4,26 @@ import java.util.regex.*;
 import java.util.Scanner;
 
 public class UserRegistration {
-	static void checkFormat() {
-		System.out.println("Enter first Name");
-		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();
+	static void checkFirstAndLast() {
 		String nameCheck = "[A-Z]{1}[a-z]{3}";
-
 		Pattern pattern = Pattern.compile(nameCheck);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Last Name");
+
+		String name = sc.nextLine();
+
 		Matcher matcher = pattern.matcher(name);
-		System.out.println(matcher.matches());
+
+		if(matcher.matches()) {
+			System.out.println("Valid input");
+		}
+		else {
+			System.out.println("Invalid input");
+		}
 		sc.close();
 	}
 	public static void main(String[] args) {
-		checkFormat();
+		checkFirstAndLast();
 	}
 }
