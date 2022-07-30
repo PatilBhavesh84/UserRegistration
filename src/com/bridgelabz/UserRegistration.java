922b27375ce60a68fb6dbb12767b2ad8d1d25fbd
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-30 15:43:04 +0530

    Added:Validation check of email

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -9,13 +9,36 @@ public class UserRegistration {
 		Pattern pattern = Pattern.compile(nameCheck);
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Last Name");
+		System.out.println("Enter First Name");
 
 		String name = sc.nextLine();
-
 		Matcher matcher = pattern.matcher(name);
 
 		if(matcher.matches()) {
+			System.out.println("Valid input");
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+		System.out.println("Enter last name");
+		String lName=sc.nextLine();
+		Matcher lastName =pattern.matcher(lName);
+		if(lastName.matches()) {
+			System.out.println("Valid input");
+		}
+		else {
+			System.out.println("Invalid input");
+		}
+	}
+	static void checkEmail() {
+		String emailCheck="[a-z]{12}+[0-9]{4}+@bl.com";
+		Pattern pattern =Pattern.compile(emailCheck);
+		System.out.println("Enter Email-Id");
+		Scanner sc=new Scanner (System.in);
+		String emailId=sc.next();
+		Matcher email= pattern.matcher( emailId);
+
+		if(email.matches()) {
 			System.out.println("Valid input");
 		}
 		else {
@@ -25,5 +48,6 @@ public class UserRegistration {
 	}
 	public static void main(String[] args) {
 		checkFirstAndLast();
+		checkEmail();
 	}
 }
