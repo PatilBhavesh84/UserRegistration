e61fa88d6ab1d4967b26baa326f2e571dbb8b422
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-06 21:44:43 +0530

    Added:Test case

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,18 +1,14 @@
 package com.bridgelabz;
 
 import java.util.regex.*;
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class UserRegistration {
-	static void checkFirstAndLast() {
+
+	public static boolean checkFirst(String fName) {
 		String nameCheck = "[A-Z]{1}[a-z]{3,7}";
 		Pattern pattern = Pattern.compile(nameCheck);
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name");
-
-		String name = sc.nextLine();
-		Matcher matcher = pattern.matcher(name);
+		Matcher matcher = pattern.matcher(fName);
 
 		if(matcher.matches()) {
 			System.out.println("Valid input");
@@ -20,41 +16,35 @@ public class UserRegistration {
 		else {
 			System.out.println("Invalid input");
 		}
-		System.out.println("Enter last name");
-		String lName=sc.nextLine();
-		Matcher lastName =pattern.matcher(lName);
-		if(lastName.matches()) {
+		return matcher.matches();
+	}
+	public static boolean checkLast(String lastName) {
+		String nameCheck= "[A-Z]{1}[a-z]{4}";
+		Pattern pattern = Pattern.compile(nameCheck);
+		Matcher lName = pattern.matcher(lastName);
+		if(lName.matches()) {
+			System.out.println("Valid Input");
+		}
+		else {
+			System.out.println("Invalid Input");
+		}
+		return lName.matches();
+	}
+	public static boolean checkEmail(String validEmailId) {
+		String emailCheck= "[abc]{3}([-+_])?([.a-zA-Z0-9]{2})?([A-Za-z0-9]{1,})?@[a-z0-9]{1,}.[a-z]{2,3}(.[a-z]{2,3})?";
+		Pattern pattern =Pattern.compile(emailCheck);
+		Matcher email= pattern.matcher( validEmailId);
+		if(email.matches()) {
 			System.out.println("Valid input");
 		}
 		else {
 			System.out.println("Invalid input");
 		}
+		return email.matches();
 	}
-	static void checkEmail() {
-		String emailCheck="[a-z]{3}+[0-9]{3}+@[a-z]{2,5}.com";
-		Pattern pattern =Pattern.compile(emailCheck);
-		//System.out.println("Enter Email-Id");
-		//Scanner sc=new Scanner (System.in);
-		String validEmailId[]= {"qwe123@gmail.com","rty213@yahoo.com",
-				"asd543@gmail.com","mbn786@yahoo.com","jkl375@gmail.com",
-				"zxs980@yahoo.com","qaz765@gmail.com","bnv345@gmail.com"};
-		for(int i=0;i<validEmailId.length;i++) {
-			Matcher email= pattern.matcher( validEmailId[i]);
-
-			if(email.matches()) {
-				System.out.println("Valid input");
-			}
-			else {
-				System.out.println("Invalid input");
-			}
-		}
-	}
-	static void checkMobileNo() {
+	public static boolean checkMobileNo(String number) {
 		String noCondition="[91]+[789]{1}+[0-9]{9}";
 		Pattern pattern =Pattern.compile(noCondition);
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter mobile number");
-		String number=sc.nextLine();
 		Matcher matcher=pattern.matcher(number);
 		if(matcher.matches()) {
 			System.out.println("Valid input");
@@ -62,15 +52,11 @@ public class UserRegistration {
 		else {
 			System.out.println("Invalid input");
 		}
-		//sc.close();
+		return matcher.matches();
 	}
-	static void checkPassword() {
-		String passCondition="[A-Z]{1}[a-z]{7}[.^$@#?&*!]{1}[0-9]{1}";
-
+	public static boolean checkPassword(String password) {
+		String passCondition="[A-Z]{1}[a-z]{6}[.^$@#?&*!]{1}[0-9]{1}";
 		Pattern pattern =Pattern.compile(passCondition);
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter password");
-		String password=sc.nextLine();
 		Matcher matcher=pattern.matcher(password);
 		if(matcher.matches()) {
 			System.out.println("Valid input");
@@ -78,12 +64,34 @@ public class UserRegistration {
 		else {
 			System.out.println("Invalid input");
 		}
-		sc.close();
+		return matcher.matches();
 	}
 	public static void main(String[] args) {
-		checkFirstAndLast();
-		checkEmail();
-		checkMobileNo();
-		checkPassword();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First name");
+		String fName =sc.nextLine();
+		checkFirst(fName);
+		System.out.println("Enter last name");
+		String lName =sc.nextLine();
+		checkLast(lName);
+
+		String validEmailId[]= {"abc@yahoo.com","abc-100@yahoo.com","abc-100@yahoo.com",
+				"abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com",
+				"abc+100@gmail.com","abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",
+				".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com",
+				"abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au","abc1..@gmail.com"};
+		for(int i=0;i<validEmailId.length;i++) {
+			checkEmail(validEmailId[i]);
+		}
+		System.out.println("Enter mobile number");
+		String number =sc.nextLine();
+		checkMobileNo(number);
+
+		System.out.println("Enter password number");
+		String password =sc.nextLine();
+		checkPassword(password);
+		sc.close();
 	}
 }
+
+
